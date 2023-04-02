fbe1091ad3b71b889e8eec12418cd0c3571c229e
Tarik Bhateja <77834838+Tarik-Bhateja@users.noreply.github.com>
2023-04-02 15:59:04 +0530

    Update views.py

--- chatbot/chatbotapp/views.py
+++ chatbot/chatbotapp/views.py
@@ -4,7 +4,7 @@ import json
 from django.shortcuts import render
  
 
-openai.api_key = "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"
+openai.api_key = ""
 
 def chatbot(request):
     if request.method == 'POST':
